b75e2cbeb05bc2aaaebd1b309b5e19b6e1bc2d6f
Deniz Can ALTUN <65543042+bendenizdeniz@users.noreply.github.com>
2024-01-21 19:57:47 +0300

    Update Program.cs

--- MassTransit.Consumer/Program.cs
+++ MassTransit.Consumer/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddMassTransit(busConfigurator =>
     busConfigurator.UsingRabbitMq((context, rabbitMqConfigurator) =>
     {
         rabbitMqConfigurator.Host(
-            new Uri("amqps://fskgqixc:r_C4CG8NFB78dEF175g_PRA7Drcbguj3@moose.rmq.cloudamqp.com/fskgqixc"));
+            new Uri("***"));
 
         rabbitMqConfigurator.ConfigureEndpoints(context);
     });
